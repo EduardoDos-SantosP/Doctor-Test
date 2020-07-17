b3086ae62703869da53670043223e415e7b941f1
EduardoDos-SantosP <66569234+EduardoDos-SantosP@users.noreply.github.com>
2020-07-17 11:18:15 -0300

    Create MaterialDownload.vue

---
+++ src/pages/MaterialDownload.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <div style="margin: 20px">Materiais para Download</div>
+    <q-btn color="primary" text-color="white" label="Botão 1" />
+    <q-btn color="primary" text-color="white" label="Botão 2" />
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "MaterialDownload"
+    }
+</script>
+
+<style scoped>
+
+</style>
